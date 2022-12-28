c63bb1874189a2b153eb41d7f86168f9f07286b5
Prachi12215421 <119073607+prachi-2627@users.noreply.github.com>
2022-12-28 10:21:36 +0530

    Add files via upload

--- class68-Exercise 6 solution in python-CipherSchools.py
+++ class68-Exercise 6 solution in python-CipherSchools.py
@@ -1,5 +1,4 @@
-import random
-winning_number = random.randint(1,100)
+winning_number = 43
 guess =  1
 number = int(input("guess a number between 1 and 100 : "))
 game_over= False
@@ -11,8 +10,9 @@ while not game_over:
     else:
         if number < winning_number:
             print("too low")
+            guess += 1
+            number = int(input("guess again : "))
         else:
             print("too high")
-        guess += 1
-        number = int(input("guess again : "))
-        # DRY - don't repeat yourself
+            guess += 1
+            number = int(input("guess again : "))
